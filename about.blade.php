b1dc691024b9d45b9eb5890115263cda852cd0e9
Madiwka <47278526+Madiwka@users.noreply.github.com>
2020-03-27 10:50:27 +0500

    Add files via upload

---
+++ about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+@section('content')
+<div class="row">
+	<div class="col-md-15">
+		<p class="quote">About Me</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-15">
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis sequi ipsum maxime ex praesentium excepturi repellat minus quae amet commodi? Dicta facere suscipit quidem, eligendi, illum quae eaque qui harum!</p>
+	</div>
+</div>
---
+++ post.blade.php
@@ -0,0 +1,13 @@
+@extends ('layouts.master')
+@section('content')
+<div class ="row">
+	<div class ="col-md-15">
+		<p class="quote"> {{$post['title']}} </p>
+	</div>
+</div>
+<div class="row">
+	<div class ="col-md-15">
+		<p>{{$post['content']}}</p>
+	</div>
+</div>	
+@endsection
